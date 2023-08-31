9e7caedfc2a94c24c65f93f66b1532c2cef62cf3
radialcoderavipoonia <radialcoderavipoonia111@gmail.com>
2023-08-31 14:58:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+function showcard1() {
+  document.getElementById("showcard1").classList.add("d_block");
+  document.getElementById("none").classList.add("d_none");
+}
+function remove1() {
+  document.getElementById("showcard1").classList.remove("d_block");
+  document.getElementById("none").classList.remove("d_none");
+}
+function showcard2() {
+  document.getElementById("showcard2").classList.add("d_block");
+  document.getElementById("none").classList.add("d_none");
+}
+function remove2() {
+  document.getElementById("showcard2").classList.remove("d_block");
+  document.getElementById("none").classList.remove("d_none");
+}
+function showcard3() {
+  document.getElementById("showcard3").classList.add("d_block");
+  document.getElementById("none").classList.add("d_none");
+}
+function remove3() {
+  document.getElementById("showcard3").classList.remove("d_block");
+  document.getElementById("none").classList.remove("d_none");
+}
+function showcard4() {
+  document.getElementById("showcard4").classList.add("d_block");
+  document.getElementById("none").classList.add("d_none");
+}
+function remove4() {
+  document.getElementById("showcard4").classList.remove("d_block");
+  document.getElementById("none").classList.remove("d_none");
+}
